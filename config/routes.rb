6227d0b4be6c8c525d03d0b886bc377487091ee6
Nasser Alqutaim <n.alqutaim@gmail.com>
2024-02-16 15:22:19 -0700

    Fix app controller to render views & fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  resources :pizzas
-  resources :toppings
+  #routes for pizzas:
+  get '/pizzas', to: 'pizzas#index', as: 'pizzas'
+  get '/pizzas/new', to: 'pizzas#new', as: 'new_pizza'
+  post '/pizzas', to: 'pizzas#create'
+  get '/pizzas/:id', to: 'pizzas#show', as: 'pizza'
+  get '/pizzas/:id/edit', to: 'pizzas#edit', as: 'edit_pizza'
+  patch '/pizzas/:id', to: 'pizzas#update'
+  put '/pizzas/:id', to: 'pizzas#update'
+  delete '/pizzas/:id', to: 'pizzas#destroy'
+
+  #routes for toppings:
+  get '/toppings', to: 'toppings#index', as: 'toppings'
+  get '/toppings/new', to: 'toppings#new', as: 'new_topping'
+  post '/toppings', to: 'toppings#create'
+  get '/toppings/:id', to: 'toppings#show', as: 'topping'
+  get '/toppings/:id/edit', to: 'toppings#edit', as: 'edit_topping'
+  patch '/toppings/:id', to: 'toppings#update'
+  put '/toppings/:id', to: 'toppings#update'
+  delete '/toppings/:id', to: 'toppings#destroy'
 end
