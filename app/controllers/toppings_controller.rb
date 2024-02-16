5ec47420e70c5d843f62224dc2d0399df4b69efa
Nasser Alqutaim <n.alqutaim@gmail.com>
2024-02-16 11:31:28 -0700

    Create actions for Toppings Controller

--- app/controllers/toppings_controller.rb
+++ app/controllers/toppings_controller.rb
@@ -1,2 +1,58 @@
 class ToppingsController < ApplicationController
+  before_action :set_topping, only: [:show, :edit, :update, :destroy]
+
+  # GET /toppings
+  def index
+    @toppings = Topping.all
+  end
+
+  # GET /toppings/1
+  def show
+  end
+
+  # GET /toppings/new
+  def new
+    @topping = Topping.new
+  end
+
+  # GET /toppings/1/edit
+  def edit
+  end
+
+  # POST /toppings
+  def create
+    @topping = Topping.new(topping_params)
+
+    if @topping.save
+      redirect_to @topping, notice: 'Topping was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /toppings/1
+  def update
+    if @topping.update(topping_params)
+      redirect_to @topping, notice: 'Topping was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /toppings/1
+  def destroy
+    @topping.destroy
+    redirect_to toppings_url, notice: 'Topping was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_topping
+      @topping = Topping.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def topping_params
+      params.require(:topping).permit(:name)
+    end
 end
