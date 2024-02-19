edd35b08b9b01ab7e7febc1288b675fda871c11a
Nasser Alqutaim <n.alqutaim@gmail.com>
2024-02-19 09:15:55 -0700

    Fix delete action for Pizzas

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,6 +1,5 @@
 class Pizza < ApplicationRecord
-  validates :name, uniqueness: true
-  has_many :pizza_toppings
-  has_many :toppings, through: :pizza_toppings
   validates :name, presence: true, uniqueness: true
+  has_many :pizza_toppings, dependent: :destroy
+  has_many :toppings, through: :pizza_toppings
 end
